6fdb4241d45d85c9a52910955b7bf1bd48e1f282
ObaidDev <abdellatif.hour.web@gmail.com>
2025-04-07 17:06:18 +0100

    Add Device Entity

---
+++ src/main/java/com/plutus360/chronologix/entities/Device.java
@@ -0,0 +1,60 @@
+package com.plutus360.chronologix.entities;
+
+import java.time.OffsetDateTime;
+import java.util.Map;
+
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "devices")
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Device {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "device_id_seq")
+    @SequenceGenerator(name = "device_id_seq", sequenceName = "device_id_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "source_device_id", nullable = false)
+    private Long sourceDeviceId;
+
+    @Column(name = "device_name", nullable = false)
+    private String deviceName;
+
+    @Column(name = "ident", nullable = false, length = 64)
+    private String ident;
+
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    private Map<String, Object> payload;
+
+    @Column(name = "event_time", nullable = false)
+    private OffsetDateTime serverTimestamp;
+
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private OffsetDateTime updatedAt;
+    
+}
