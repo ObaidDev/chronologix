15ecc6e82e32a7129a748c97e79fb4b4119f0dc3
ObaidDev <abdellatif.hour.web@gmail.com>
2025-04-07 17:06:35 +0100

    Add DeviceService

---
+++ src/main/java/com/plutus360/chronologix/service/DeviceService.java
@@ -0,0 +1,59 @@
+package com.plutus360.chronologix.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.plutus360.chronologix.dao.repositories.DeviceRepo;
+import com.plutus360.chronologix.dtos.DeviceRequest;
+import com.plutus360.chronologix.dtos.DeviceResponse;
+import com.plutus360.chronologix.entities.Device;
+import com.plutus360.chronologix.mapper.DeviceMapper;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Transactional
+public class DeviceService {
+
+
+    private DeviceMapper deviceMapper;
+
+    private DeviceRepo deviceRepo;
+
+
+
+    @Autowired
+    DeviceService(
+        DeviceMapper deviceMapper,
+        DeviceRepo deviceRepo
+    ) {
+        this.deviceMapper = deviceMapper;
+        this.deviceRepo = deviceRepo;
+    }
+
+
+
+
+    public List<DeviceResponse> createEntities(List<DeviceRequest> deviceRequests) {
+
+        log.info("Creating groups in batch...");
+        
+
+        List<Device> devices = deviceRepo.insertInBatch(deviceMapper.toDeviceList(deviceRequests) ) ;
+
+        log.debug("service : groups : {}" , devices);
+
+        List<DeviceResponse> deviceResponses = deviceMapper.toDeviceResponseList(devices);
+
+        log.debug("service : DeviceResponses : {}" , deviceResponses);
+
+        return deviceResponses ;
+        
+    }
+
+    
+}
