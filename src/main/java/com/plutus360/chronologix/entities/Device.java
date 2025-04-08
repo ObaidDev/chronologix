83a65d091fdec6720262ca8a198cbc15ca2caf99
ObaidDev <abdellatif.hour.web@gmail.com>
2025-04-08 10:32:02 +0100

    Update Device remove unsed imports

--- src/main/java/com/plutus360/chronologix/entities/Device.java
+++ src/main/java/com/plutus360/chronologix/entities/Device.java
@@ -13,7 +13,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
