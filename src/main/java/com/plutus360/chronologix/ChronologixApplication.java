8baa2d1f9483370151384dbfdb21575f5fe40d1b
ObaidDev <abdellatif.hour.web@gmail.com>
2025-04-10 12:19:50 +0100

    EnableCache In Application

--- src/main/java/com/plutus360/chronologix/ChronologixApplication.java
+++ src/main/java/com/plutus360/chronologix/ChronologixApplication.java
@@ -2,8 +2,10 @@ package com.plutus360.chronologix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ChronologixApplication {
 
 	public static void main(String[] args) {
