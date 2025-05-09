3588e70efddb2ffd777e20c57b0b3489798678ce
ObaidDev <abdellatif.hour.web@gmail.com>
2025-05-09 17:11:05 +0100

    Add AuthenticationController

---
+++ src/main/java/com/plutus360/chronologix/web/AuthenticationController.java
@@ -0,0 +1,48 @@
+package com.plutus360.chronologix.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.plutus360.chronologix.dtos.LoginResponse;
+import com.plutus360.chronologix.dtos.LoginUserDto;
+import com.plutus360.chronologix.dtos.RegisterUserDto;
+import com.plutus360.chronologix.entities.User;
+import com.plutus360.chronologix.service.AuthenticationService;
+import com.plutus360.chronologix.service.JwtService;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+    private final JwtService jwtService;
+    
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signup(registerUserDto);
+
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+
+        LoginResponse loginResponse = LoginResponse.builder()
+                .token(jwtToken)
+                .expiresIn(jwtService.getExpirationTime())
+                .build();
+
+        return ResponseEntity.ok(loginResponse);
+    }
+}
