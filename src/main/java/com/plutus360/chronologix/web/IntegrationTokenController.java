2908ac576e4526abca516b4fcc0239aa17a79574
ObaidDev <abdellatif.hour.web@gmail.com>
2025-05-12 12:28:16 +0100

    Update IntegrationTokenController

--- src/main/java/com/plutus360/chronologix/web/IntegrationTokenController.java
+++ src/main/java/com/plutus360/chronologix/web/IntegrationTokenController.java
@@ -3,15 +3,19 @@ package com.plutus360.chronologix.web;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.plutus360.chronologix.dtos.IntegrationTokenRequest;
+import com.plutus360.chronologix.dtos.IntegrationTokenResponse;
 import com.plutus360.chronologix.service.IntegrationTokenService;
 
 import jakarta.validation.Valid;
@@ -40,6 +44,12 @@ public class IntegrationTokenController {
 
         return integrationTokenService.createEntities(integrationTokenRequests);
 
+    }
+
+    @GetMapping("/{ids}")
+    public List<IntegrationTokenResponse> findByIds (@PathVariable List<Long> ids) {
+
+        return integrationTokenService.findByIds(ids) ;
     }
     
 }
