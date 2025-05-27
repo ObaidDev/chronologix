ce7c27044da7ffc0148b010cff5229e3498b0ad3
ObaidDev <abdellatif.hour.web@gmail.com>
2025-05-27 13:13:16 +0100

    Add AuthConfig class for authentication configuration and user details service

---
+++ src/main/java/com/plutus360/chronologix/conf/AuthConfig.java
@@ -0,0 +1,57 @@
+package com.plutus360.chronologix.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.plutus360.chronologix.dao.repositories.UserRepo;
+
+@Configuration
+public class AuthConfig {
+
+
+    private final UserRepo userRepository;
+
+
+    AuthConfig(UserRepo userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+}
