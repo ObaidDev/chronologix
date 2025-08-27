beae000e7612f001907470cb04caf3692f90f113
ObaidDev <abdellatif.hour.web@gmail.com>
2025-08-27 12:10:03 +0100

    Add deleteToken method to IntegrationTokenService for cache eviction

--- src/main/java/com/plutus360/chronologix/service/IntegrationTokenService.java
+++ src/main/java/com/plutus360/chronologix/service/IntegrationTokenService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -87,6 +88,11 @@ public class IntegrationTokenService {
             .toList();
     }
 
+
+    @CacheEvict(value = "integrationTokens", key = "#token") // delete the token from cache 
+    public boolean deleteToken(String token) {
+       return integrationTokenRepo.deleteToken(token);
+    }
 
 
     
