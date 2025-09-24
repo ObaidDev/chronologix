e704f011598e65593e7e201c0ff67be35c47652d
ObaidDev <abdellatif.hour.web@gmail.com>
2025-09-24 15:29:30 +0100

    Add EmailService and implement password reset endpoints in AuthenticationController

--- src/main/java/com/plutus360/chronologix/web/AuthenticationController.java
+++ src/main/java/com/plutus360/chronologix/web/AuthenticationController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.plutus360.chronologix.dtos.LoginResponse;
@@ -12,6 +13,8 @@ import com.plutus360.chronologix.dtos.RegisterUserDto;
 import com.plutus360.chronologix.entities.User;
 import com.plutus360.chronologix.service.AuthenticationService;
 import com.plutus360.chronologix.service.JwtService;
+import com.plutus360.chronologix.service.PasswordResetService;
+import com.plutus360.chronologix.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,10 +26,19 @@ public class AuthenticationController {
     private final JwtService jwtService;
     
     private final AuthenticationService authenticationService;
+    private final PasswordResetService passwordResetService;
+    private final UserService userService ;
 
-    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+    public AuthenticationController(
+        JwtService jwtService, 
+        AuthenticationService authenticationService ,
+        PasswordResetService passwordResetService ,
+        UserService userService
+    ) {
         this.jwtService = jwtService;
         this.authenticationService = authenticationService;
+        this.passwordResetService = passwordResetService;
+        this.userService = userService ;
     }
 
     @PostMapping("/signup")
@@ -51,5 +63,38 @@ public class AuthenticationController {
                 .build();
 
         return ResponseEntity.ok(loginResponse);
+    }
+
+
+
+
+
+
+
+    /*
+     * 
+     * Password fogeten
+     */
+
+
+    /**
+     * Step 1: User requests password reset
+     */
+    @PostMapping("/forgot-password")
+    public String forgotPassword(@RequestParam String email) {
+       
+        return passwordResetService.forgotPassword(email);
+    }
+
+
+
+    /**
+     * Step 2: User submits new password with token
+     */
+    @PostMapping("/reset-password")
+    public String resetPassword(@RequestParam String token,
+                                                @RequestParam String newPassword) {
+
+        return passwordResetService.resetPassword(token, newPassword);
     }
 }
